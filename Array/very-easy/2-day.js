ac4a71d39440cdb6937d3ddac5642bb04af8cd81
tohidkhan508 <tohidkhann508@gmail.com>
2026-05-15 03:06:50 +0530

    continue question

--- Array/very-easy/2-day.js
+++ Array/very-easy/2-day.js
@@ -271,63 +271,17 @@
 // }
 
 //! 6 => Example
-function concat(arr1, arr2) {
-  return arr2.reduce(
-    (acc, curr) => {
-      acc.push(curr);
-      return acc;
-    },
-    [...arr1],
-  );
-}
+// function concat(arr1, arr2) {
+//   return arr2.reduce(
+//     (acc, curr) => {
+//       acc.push(curr);
+//       return acc;
+//     },
+//     [...arr1],
+//   );
+// }
 
 //! Examples
-console.log(concat([1, 3, 5], [2, 6, 8])); //* ➞ [1, 3, 5, 2, 6, 8]
-console.log(concat([7, 8], [10, 9, 1, 1, 2])); //* ➞ [7, 8, 10, 9, 1, 1, 2]
-console.log(concat([4, 5, 1], [3, 3, 3, 3, 3])); //* ➞ [4, 5, 1, 3, 3, 3, 3, 3]
-
-// 11. Find the Index
-// Create a function that takes an array and a string as arguments and returns the index of the string.
-
-// function findIndex(arr, word) {
-//     return arr.indexOf(word);
-// }
-
-// function findIndex(arr, word) {
-//     for(let i = 0; i < arr.length; i++) {
-//         if(arr[i] === word) {
-//             return i;
-//         }
-//     }
-// }
-
-// function findIndex(arr, word) {
-//     let index = -1;
-//     arr.forEach((item, i) => {
-//         if(item === word) {
-//             index = i;
-//         }
-//     })
-//     return index;
-// }
-
-// function findIndex(arr, word) {
-//     return arr.findIndex(item => item === word);
-// }
-
-// function findIndex(arr, word) {
-//     let index = 0;
-//     for(let item of arr) {
-//         if(item === word) {
-//             return index;
-//         }
-//         index++;
-//     }
-//     return -1;
-// }
-
-// Examples
-// console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh")) // ➞ 2
-// console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue")) // ➞ 1
-// console.log(findIndex(["a", "g", "y", "d"], "d")) // ➞ 3
-// console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple")) // ➞ 0
+// console.log(concat([1, 3, 5], [2, 6, 8]));        //* ➞ [1, 3, 5, 2, 6, 8]
+// console.log(concat([7, 8], [10, 9, 1, 1, 2]));    //* ➞ [7, 8, 10, 9, 1, 1, 2]
+// console.log(concat([4, 5, 1], [3, 3, 3, 3, 3]));  //* ➞ [4, 5, 1, 3, 3, 3, 3, 3]
--- Array/very-easy/3-day.js
+++ Array/very-easy/3-day.js
@@ -1,192 +1,307 @@
-// 12. Array Indexing
-// Given an index and an array, return the value of the array with the given index.
+//? 11. Find the Index
+//? Create a function that takes an array and a string as arguments and returns the index of the string.
 
-// function valueAt(arr, num) {
-//     return arr[Math.floor(num)];
+//! 1 => Example
+// function findIndex(arr, str) {
+//   return arr.indexOf(str);
 // }
 
-// function valueAt(arr, num) {
-//     let result;
-//     let idx = Math.floor(num)
-//     for(let i = 0; i < arr.length; i++) {
-//         if(i === idx) {
-//             result = arr[i] 
-//         }
+//! 2 => Example
+// function findIndex(arr, str) {
+//   return arr.findIndex((val) => val === str);
+// }/
+
+//! 3 => Example
+// function findIndex(arr, str) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === str) {
+//       return i;
 //     }
-//     return result;
+//   }
 // }
 
-// function valueAt(arr, num) {
-//     let result;
-//     let idx = Math.floor(num);
-//     let i = 0;
-//     for(let value of arr) {
-//         if(i === idx) {
-//             result = value;
-//         }
-//         i++
+//! 4 => Example
+// function findIndex(arr, str) {
+//   let i = 0;
+//   for (let val of arr) {
+//     if (val === str) {
+//       return i;
 //     }
-//     return result;
+//     i++;
+//   }
 // }
 
+//! 5 => Example
+// function findIndex(arr, str) {
+//     let result = -1
+//   arr.forEach((val, i) => {
+//     if (val === str && result === -1) {
+//       result = i;
+//     }
+//   });
+//   return result
+// }
+
+//! 6 => Example
+// function findIndex(arr, str) {
+//   return arr.reduce((acc, curr, i) => {
+//     if (curr === str) {
+//       return i;
+//     }
+//     return acc
+//   }, -1);
+// }
+
+//! Examples
+// console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh"));                  //* ➞ 2
+// console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue"));               //* ➞ 1
+// console.log(findIndex(["a", "g", "y", "d"], "d"));                              //* ➞ 3
+// console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple")); //* ➞ 0
+
+//? 12. Array Indexing
+//? Given an index and an array, return the value of the array with the given index.
+
+//! 1 => Example
+// function valueAt(arr, num) {
+//     return arr[Math.floor(num)]
+// }
+
+//! 2 => Example
 // function valueAt(arr, num) {
 //     return arr[Math.trunc(num)]
 // }
 
+//! 3 => Example
 // function valueAt(arr, num) {
-//     return arr.at(Math.floor(num));
+//     return arr.at(num)
 // }
 
-// // Examples
-// console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2)) // ➞ 6
-// console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2)) // ➞ 5
-// console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2)) // ➞ 4
-
-
-
-
-// 13. Find the Index (Part 1)
-// Create a function that finds the index of a given item.
-
-// function search(arr, num) {
-//     return arr.indexOf(num);
-// }
-
-// function search(arr, num) {
-//     let result = -1;
-//     for(let i = 0; i < arr.length; i++) {
-//         if(arr[i] === num) {
-//             return i
-//         }
+//! 4 => Example
+// function valueAt(arr, num) {
+//   let result;
+//   let idx = Math.floor(num);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i === idx) {
+//       result = arr[i];
 //     }
-//     return result;
+//   }
+//   return result;
 // }
 
-// function search(arr, num) {
-//     let result = -1;
-//     arr.forEach((arr1, value) => {
-//         if(arr1 === num) {
-//             result = value
-//         }
-//     });
-//     return result
-// }
+//! 5 => Example
+// function valueAt(arr, num) {
+//   let result;
+//   let idx = Math.floor(num);
+//   let i = 0;
 
-// function search(arr, num) {
-//     return arr.findIndex((value) => value == num)
-// }  
-
-// function search(arr, num) {
-//     let res = 0;
-//     for(let value of arr) {
-//         if(value === num) {
-//             return res;
-//         }
-//         res++;
+//   for (let val of arr) {
+//     if (i === idx) {
+//       result = val;
 //     }
-//     return -1
+//     i++;
+//   }
+//   return result;
 // }
 
-// Examples
-// console.log(search([1, 5, 3], 5)) // ➞ 1
-// console.log(search([9, 8, 3], 3)) // ➞ 2
-// console.log(search([1, 2, 3], 4)) // ➞ -1
+//! 6 => Example
+// function valueAt(arr, num) {
+//   let result;
+//   let idx = Math.floor(num);
 
-
-
-
-// 14. Buggy Code (Part 3)
-// Fix t/he code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
-
-// function sumArray(arr) {
-//     return arr.reduce((num1, num2) => num1 + num2);
-// }
-
-// function sumArray(arr) {
-//     let result = 0;
-//     for(let value of arr) {
-//         result += value
+//   arr.forEach((data, i) => {
+//     if (i === idx) {
+//       result = data;
 //     }
-//     return result
+//   });
+//   return result;
 // }
 
-// function sumArray(arr) {
-//     let result = 0;
-//     for(let i = 0; i < arr.length; i++) {
-//         result += arr[i];
-//     }
-//     return result;
+//! 7 => Example
+// function valueAt(arr, num) {
+//   let idx = Math.floor(num);
+//   return arr.reduce((acc, curr, i) => {
+//     if (i === idx) return curr;
+//     return acc;
+//   }, -1);
 // }
 
-// function sumArray(arr) {
-//     let sum = 0;
-//     arr.map((num) => sum += num);
-//     return sum;
-// }
+//! Examples
+// console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2));     //* ➞ 6
+// console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2));    //* ➞ 5
+// console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2)) ; //* ➞ 4
 
-// function sumArray(arr) {
-//     let sum = 0;
-//     arr.forEach((num) => sum += num);
-//     return sum;
-// }
+//? 13. Find the Index (Part 1)
+//? Create a function that finds the index of a given item.
 
-// // Examples
-// console.log(sumArray([1, 2, 3, 4, 5])) // ➞ 15
-// console.log(sumArray([-1, 0, 1])) // ➞ 0
-// console.log(sumArray([0, 4, 8, 12])) // ➞ 24
-
-
-
-
-// 15. Find the Index (Part #2)
-// Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
-
+//! 1 => Example
 // function search(arr, num) {
 //     return arr.indexOf(num)
 // }
 
+//! 2 => Example
 // function search(arr, num) {
-//     let result = 0;
-//     for(let value of arr) {
-//         if(value === num) {
-//             return result;
-//         }
-//         result++;
-//     }
-//     return -1
+//     return arr.findIndex(value => value == num)
 // }
 
+//! 3 => Example
 // function search(arr, num) {
-//     let result = -1;
-//     for(let i = 0; i < arr.length; i++) {
-//         if(arr[i] === num) {
-//             return i;
-//         }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === num) {
+//       return i;
 //     }
+//   }
+//   return -1;
+// }/
+
+//! 4 => Example
+// function search(arr, num) {
+//   let res = -1;
+//   let i = 0;
+
+//   for (let val of arr) {
+//     if (val === num) {
+//       return i;
+//     }
+//     i++;
+//   }
+//   return res;
+// }
+
+//! 5 => Exmaple
+// function search(arr, num) {
+//   let result = -1;
+//   arr.forEach((val, i) => {
+//     if (val === num) {
+//       result = i;
+//     }
+//   });
+//   return result;
+// }
+
+//! 6 => Example
+// function search(arr, num) {
+//   return arr.reduce((acc, curr, i) => {
+//     if (curr === num) return i;
+//     return acc;
+//   }, -1);
+// }
+
+//! Examples
+// console.log(search([1, 5, 3], 5)); //* ➞ 1
+// console.log(search([9, 8, 3], 3)); //* ➞ 2
+// console.log(search([1, 2, 3], 4)); //* ➞ -1
+
+//? 14. Buggy Code (Part 3)
+//? Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+//! 1 => Example
+// function sumArray(arr) {
+//     return arr.reduce((num1, num2) => num1 + num2)
+// }
+
+//! 2 => Example
+// function sumArray(arr) {
+//   let result = 0
+
+//   for (let i = 0; i < arr.length; i++) {
+//     result += arr[i];
+//   }
+//   return result;
+// }
+
+//! 3 => Example
+// function sumArray(arr) {
+//   let result = 0;
+
+//   for (let val of arr) {
+//     result += val;
+//   }
+//   return result;
+// }
+
+//! 4 => Example
+// function sumArray(arr) {
+//     let result = 0;
+//     arr.forEach(val => {
+//         result += val
+//     });
 //     return result
 // }
 
-// function search(arr, num) {
-//     let result = -1;
-//     arr.forEach((value, idx) => {
-//         if(value === num) {
-//             result = idx;
-//         }
-//     });
-//     return result;
+//! 5 => Example
+// function sumArray(arr) {
+//   return arr.reduce((acc, curr) => {
+//     acc += curr;
+//     return acc;
+//   }, 0);
 // }
 
+//! Examples
+// console.log(sumArray([1, 2, 3, 4, 5])); //* ➞ 15
+// console.log(sumArray([-1, 0, 1]));      //* ➞ 0
+// console.log(sumArray([0, 4, 8, 12]));   //* ➞ 24
+
+//? 15. Find the Index (Part #2)
+//? Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+
+//! 1 => Example
+// function search(arr, num) {
+//     return arr.indexOf(num)
+// }
+
+//! 2 => Example
 // function search(arr, num) {
 //     return arr.findIndex(value => value === num)
 // }
 
-// // Examples
-// console.log(search([1, 2, 3, 4], 3)) // ➞ 2
-// console.log(search([2, 4, 6, 8, 10], 8)) // ➞ 3
-// console.log(search([1, 3, 5, 7, 9], 11)) // ➞ -1
+//! 3 => Example
+// function search(arr, num) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === num) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
+//! 4 => Example
+// function search(arr, num) {
+//   let res = -1;
+//   let i = 0;
 
+//   for (let val of arr) {
+//     if (val === num) {
+//       return i;
+//     }
+//     i++;
+//   }
+//   return res;
+// }
 
+//! 5 => Example
+// function search(arr, num) {
+//   let result = -1;
+
+//   arr.forEach((val, i) => {
+//     if (val === num) {
+//       result = i;
+//     }
+//   });
+//   return result;
+// }
+
+//! 6 => Example
+function search(arr, num) {
+  return arr.reduce((acc, curr, i) => {
+    if (curr === num) return i;
+    return acc;
+  }, -1);
+}
+
+//! Examples
+console.log(search([1, 2, 3, 4], 3)); // ➞ 2
+console.log(search([2, 4, 6, 8, 10], 8)); // ➞ 3
+console.log(search([1, 3, 5, 7, 9], 11)); // ➞ -1
 
 // 16. Check if an Array Contains a Given Number
 // Write a function to check if an array contains a particular number.
@@ -250,9 +365,6 @@
 // console.log(check([1, 1, 2, 1, 1], 3)) // ➞ false
 // console.log(check([5, 5, 5, 6], 5)) // ➞ true
 // console.log(check([], 5)) // ➞ false
-
-
-
 
 // 17. Less Than, Greater Than
 // Create a function that takes two numbers num1, num2, and an array arr and returns an array containing all the numbers in arr greater than num1 and less than num2.
